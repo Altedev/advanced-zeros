c6611b8355050ec0086c3d19e92b091aa77a63f9
altedev <altedev@yandex.ru>
2019-03-20 18:10:07 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+    let factorial = [];
+    let k = 2;
+    while (k < base) {
+        while (base % k === 0) {
+            factorial.push(k);
+            base /= k;
+        }
+        k += 1;
+    }
+    if (base > 1) {
+        factorial.push(base);
+    }
+    factorial.sort((a, b) => a - b);
+
+    let zeros = [];
+    let calculate = 1;
+    for (let i = 0; i < factorial.length; i += calculate) {
+        let counter = 0;
+        for (let j = i; j < factorial.length; j += 1) {
+            if (factorial[i] === factorial[j]) {
+                counter += 1;
+            } else {
+                calculate = counter;
+                break;
+            }
+        }
+        let result = 0;
+        let j = factorial[i];
+        while (true) {
+            result += (number - number % j) / j;
+            j *= factorial[i];
+            if (j > number) {
+                break;
+            }
+        }
+        zeros.push((result - result % counter) / counter);
+    }
+
+    return Math.min.apply(Math, zeros);
+};
